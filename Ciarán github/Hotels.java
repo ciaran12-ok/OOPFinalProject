4fb77f290dbb032ac58132de3ed1f09aa2387737
ciaran12-ok <93784853+ciaran12-ok@users.noreply.github.com>
2021-11-16 20:31:46 +0000

    Add files via upload

---
+++ Ciarán github/Hotels.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ciaranokeeffe
+ */
+public class Hotels {
+    
+    private String name;
+    private int star;
+    private Room starArray[];
+    private Room roomForCon;
+    
+    
+    public Hotels(String name,int starNo, Room pStarArray[]){  
+        this.name = name;
+        this.star= starNo;
+        pStarArray = starArray;
+   
+        }
+    
+    Hotel BallyfinHouse = new Hotel("Ballyfin House", 5, );
+}
